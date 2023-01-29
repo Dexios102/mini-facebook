6dada9fca8f5576614ea64c668610f84f2fa2633
de Villa, Marcous Alexis C <82773092+devillamarcousalexis@users.noreply.github.com>
2023-01-29 16:33:29 +0800

    Landing Page - Marcous

---
+++ frontend/src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeHeader from './Components/HomeHeader';
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <HomeHeader />
+    </div>
+  );
+}
+
+export default LandingPage;
