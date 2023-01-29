8596a42ba38cccafbf3be1b52877805914f7e1e0
Dexios102 <119906753+Dexios102@users.noreply.github.com>
2023-01-29 22:20:53 +0800

    de

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,17 +1,43 @@
-import { Grid } from "@mui/material";
+import React from "react";
+import { TextField, Button } from '@mui/material';
 
-const Contact = () => {
-  return <>
-    <Grid container justifyContent='center'>
-      <Grid item sm={10}>
-        <h1>Contact Page</h1>
-        <hr />
-        <p>Contact Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum officiis debitis vel tenetur quos animi vero voluptates reiciendis, omnis sed in libero temporibus deleniti pariatur expedita corporis officia. Odit enim, quasi facere magnam earum officiis ipsa aliquid impedit velit quibusdam dolor ex esse ratione explicabo quod, culpa temporibus? Dolorem deleniti doloremque maxime quas deserunt. Ex aspernatur saepe illo eaque corrupti placeat, aperiam nulla adipisci itaque quos necessitatibus iure at minus non delectus ratione quod ad. Alias dolore perferendis est expedita iure! Nostrum laborum tempore amet commodi voluptas accusamus enim repudiandae, quia odio cumque, laboriosam architecto illo! Aliquid, fuga quis.</p>
-      </Grid>
-    </Grid>
-  </>;
+const ContactPage = () => {
+  return (
+    <>
+      <form noValidate autoComplete="off">
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Message"
+          multiline
+          rows={4}
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        <Button variant="contained" color="primary" fullWidth>
+          Submit
+        </Button>
+      </form>
+      <p>
+        You can also email us directly at{" "}
+        <a href="mailto:developer@example.com">developer@example.com</a>
+      </p>
+    </>
+  );
 };
 
-export default Contact;
-
-
+export default ContactPage;
